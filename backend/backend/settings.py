3238f56da9389b9b2c2689fbeb435989cbf4c71a
Heaven-Bro <heavenchakma04@gmail.com>
2026-06-22 22:35:25 +0600

    Update database settings to use PostgreSQL and add URL configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'skillswap',
+        'USER': 'postgres',
+        'PASSWORD': 'BejeGulo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
