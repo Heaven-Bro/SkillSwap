9883ec402c4a472597724810b5a62f2e12f83605
Heaven-Bro <heavenchakma04@gmail.com>
2026-06-23 21:49:04 +0600

    Add Skill model with fields and migration

--- backend/skills/models.py
+++ backend/skills/models.py
@@ -13,3 +13,34 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Skill(models.Model):
+    LEVEL_CHOICES = (
+        ("Beginner", "Beginner"),
+        ("Intermediate", "Intermediate"),
+        ("Advanced", "Advanced"),
+    )
+
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    Category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="skills"
+    )
+
+    ower_name = models.CharField(max_length=100)
+
+    exprience_level = models.CharField(
+        max_length=20,
+        choices=LEVEL_CHOICES
+    )
+
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
