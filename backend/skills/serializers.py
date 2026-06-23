c4780f0c3e5f834c1b50d250dcda14e55275b609
Heaven-Bro <heavenchakma04@gmail.com>
2026-06-23 21:53:12 +0600

    Add Skill serializer and viewset for handling skills data

--- backend/skills/serializers.py
+++ backend/skills/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Skill
 
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
+        fields = "__all__"
+
+class SkillSerializer(serializers.ModelSerializer):
+
+    category_name = serializers.CharField(
+        source = "category.name",
+        read_only = True
+    )
+
+    class Meta:
+        model = Skill 
         fields = "__all__"
--- backend/skills/views.py
+++ backend/skills/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Skill
+from .serializers import CategorySerializer, SkillSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+
+    queryset = Skill.objects.select_related("category").all()
+
+    serializer_class = SkillSerializer
